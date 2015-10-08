4f7e4d418b52360e99fd087b505e9c4d8e5b87b0
sunmanli <sml@tju.edu.cn>
2015-10-09 00:39:02 +0800

    添加chain.cpp

---
+++ chain/chain.cpp
@@ -0,0 +1,42 @@
+#include "chain.h"
+
+void main()
+{
+    char str[]="1C++ 2FORTRAN 3PASCAL 4BASIC";
+	Chain<char> L;
+	int i;
+
+	try
+	{
+        for(i=0; str[i] != '\0'; i++)
+        {
+            L.Insert(i, str[i]);
+        }
+	}
+	catch(const out_of_range &exc)
+	{
+		cout<<exc.what()<<endl;
+	}
+
+	cout<<L<<endl;
+
+	i=0;
+	char x;
+
+	while( i < L.Length() )
+	{
+		L.Find(i, x);
+
+		if ( (x >= '0') && (x <= '9') )
+		{
+			L.Delete(i, x);
+		}
+		else
+		{
+			i++;
+		}
+	}
+
+    cout<<L<<endl;
+	system("pause");
+}
